51283dba0b81933abd681b49acb32121cf135c96
HabibaKader <habibakader@outlook.com>
2026-04-22 20:41:36 +0200

    Add project files.

---
+++ Assignment04_EntityFrameworkCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment04_EntityFrameworkCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
