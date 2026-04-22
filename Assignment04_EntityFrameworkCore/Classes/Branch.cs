34fe9dc634121de7db0dd110b6c08b123423346e
HabibaKader <habibakader@outlook.com>
2026-04-22 20:47:20 +0200

    Classes (Branch,Manager,Customer,Account,Transaction,AccountCustomer)

---
+++ Assignment04_EntityFrameworkCore/Classes/Branch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04_EntityFrameworkCore.Classes
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public int ManagerId { get; set; }
+        public Manager Manager { get; set; }
+
+        public ICollection<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
